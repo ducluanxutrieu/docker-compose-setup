75a8e81d9ca6824d7a0f193d1ad0340bcf770b90
Tran Duc Luan <ducluanxutrieu@gmail.com>
2021-02-17 12:34:47 +0700

    Change install docker

--- install-docker.sh
+++ install-docker.sh
@@ -14,6 +14,12 @@ sudo add-apt-repository \
    stable" -y
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+git clone https://github.com/DevOps01MoocProject/PartyWebServer.git
+cd PartyWebServer
+sudo docker build -t ducluanxutrieu/node_party_web_server .
+sudo docker run -p 49160:3005 -d ducluanxutrieu/node_party_web_server
+
 echo "Install docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -26,7 +32,3 @@ export GITLAB_HOME=/srv/gitlab
 sudo sysctl -w vm.max_map_count=262144
 sudo echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
 sudo docker-compose up -d
-git clone https://github.com/DevOps01MoocProject/PartyWebServer.git
-cd PartyWebServer
-sudo docker build -t ducluanxutrieu/node_party_web_server .
-sudo docker run -p 49160:3005 -d ducluanxutrieu/node_party_web_server
